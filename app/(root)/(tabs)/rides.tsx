055c45b0b146f731ecd064991a145207af7e8f80
Erik Hebeler <erikhebeler@uol.com.br>
2025-08-14 23:39:50 -0300

    Initial Push

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,13 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Rides = () => {
+    return (
+        <SafeAreaView className="flex-1 items-center justify-center bg-white">
+            <Text className="text-xl font-bold text-blue-500">Rides in Tabs</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Rides;
+
